c9061c9427100ebfbc833e863bfa35ae8484b3ec
khanhlk20 <58789154+khanhlk20@users.noreply.github.com>
2019-12-15 22:33:12 +0700

    Nevigtion Vew

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,44 +1,95 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
+
 
 public class HomeActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-    private Button button;
-    @Override
 
+    private Button settig,help,button;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle drawerToggle;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        button = findViewById(R.id.button);
+        button=findViewById(R.id.button);
+        settig=findViewById(R.id.setting);
+        help=findViewById((R.id.help));
+        settig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginactivity = new Intent(HomeActivity.this,PostActivity.class);
+                startActivity(loginactivity);
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 mAuth.signOut();
                 Intent loginactivity = new Intent(HomeActivity.this,LoginActivity.class);
                 startActivity(loginactivity);
 
             }
         });
+        drawerLayout = findViewById(R.id.homeactivity);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
     }
-    public boolean onCreateOptionsMenu(Menu menu){
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
+
         return super.onCreateOptionsMenu(menu);
     }
-    public boolean onOptionsItemSelected(MenuItem item){
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.action_add){
             startActivity(new Intent(HomeActivity.this, PostActivity.class));
         }
-        return  super.onOptionsItemSelected(item);
+
+
+        if(drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
+
